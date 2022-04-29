d17c56d18c8e1435af2193ce66866e41c83d90b9
Jay <jayjogi444@gmail.com>
2022-04-29 15:13:20 +0530

    sql solved

--- upcr.php
+++ upcr.php
@@ -11,8 +11,8 @@ if ($a == "add") {
     
     $course = $_POST['b'];
     $sql = "INSERT INTO course(course) VALUES ('$course')"or die("ERROR: Data no stored in database.".mysqli_error($conn));
-    echo $sql;
-    //mysqli_query($conn, $sql);
+    //echo $sql;
+    mysqli_query($conn, $sql);
    
   
     
@@ -21,8 +21,8 @@ if ($a == "add") {
 if ($a == "del") {
     $course_id = $_POST['c'];
     $sql = "DELETE FROM course WHERE course_id = $course_id" or die("ERROR: Data no stored in database.".mysqli_error($conn));
-   echo $sql;
-    //mysqli_query($conn, $sql);
+    //echo $sql;
+    mysqli_query($conn, $sql);
   
 
    
@@ -38,7 +38,7 @@ if ($a == "update") {
     
     $sql = "UPDATE course SET course = '$course' WHERE course_id = $id";// or die("ERROR: Data not stored in database.".mysqli_error($conn));
     echo $sql;  
-    //mysqli_query($conn, $sql);
+    mysqli_query($conn, $sql);
      
 
   
