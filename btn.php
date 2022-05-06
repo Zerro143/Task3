1a54d78d6013e79e81d27c5a11b684195c96a910
Jay <jayjogi444@gmail.com>
2022-05-06 16:47:06 +0530

    added show and hide buttons when the form is opened

--- btn.php
+++ btn.php
@@ -5,7 +5,7 @@
                
             <br>    
             
-            <button class="btn-xs btn-primary" onclick="location.href='students.php'">Show All Students Record</button>
+            <button class="btn-xs btn-primary" onclick="location.href='students.php' ">Show All Students Record</button>
             <button class="btn-xs btn-primary" onclick="location.href='course.php'">Show All course Record</button>
             <button class="btn-xs btn-primary" id="studentAdd">Add Student Record</button>
             <button class="btn-xs btn-primary" id="openForm">Add Course</button>
