cf818d9c7c218d28ee2dc0b326bda568bef8aed6
Jay <jayjogi444@gmail.com>
2022-05-06 18:36:19 +0530

    added csv logic

--- assets/js/saction.js
+++ assets/js/saction.js
@@ -416,6 +416,36 @@ $(document).ready(function(){
 
 
     });
+    $('#upload_csv_form').on("submit", function(e){  
+        e.preventDefault(); 
+        $.ajax({  
+             url:"csvst.php",  
+             method:"POST",  
+             data:new FormData(this),  
+             contentType:false,          // The content type used when sending data to the server.  
+             cache:false,                // To unable request pages to be cached  
+             processData:false,          // To send DOMDocument or non processed data file it is set to false  
+             success: function(data){  
+                  if(data=='Error1')  
+                  {  
+                       alert("Invalid File");  
+                  }  
+                  else if(data == "Error2")  
+                  {  
+                       alert("Please Select File");  
+                  }                           
+                  else if(data == "Success")  
+                  {  
+                       alert("CSV file data has been imported");  
+                       $('#upload_csv_form')[0].reset();
+                  }  
+                  else  
+                  {  
+                      // $('#employee_table').html(data);  
+                  }  
+             }  
+        })  
+   });  
    
     
 });
